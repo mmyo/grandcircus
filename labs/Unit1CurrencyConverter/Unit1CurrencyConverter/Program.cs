0a428034b51edfa9838ce45eea8606e01a02391b
Matthew Yo <matthewyo@quickenloans.com>
2019-03-26 20:43:16 -0400

    first commit

---
+++ labs/Unit1CurrencyConverter/Unit1CurrencyConverter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Unit1CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter first number");
+                double firstNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter third number");
+                double thirdNumber = double.Parse(Console.ReadLine());
+
+                //calculate average of three numbers
+                double averageOfThreeNumbers = (firstNumber + secondNumber + thirdNumber) / 3;
+                double totalOfThreeNumbers = firstNumber + secondNumber + thirdNumber;
+
+                Console.WriteLine(); //add a space to make it easier to read
+
+                Console.WriteLine("Here's the average: " + averageOfThreeNumbers);
+                Console.WriteLine("Here's the lowest: " + Math.Min(firstNumber, (Math.Min(secondNumber, thirdNumber))));
+                Console.WriteLine("Here's the highest: " + Math.Max(firstNumber, (Math.Max(secondNumber, thirdNumber))));
+
+                Console.WriteLine(); //add a space to make it easier to read
+
+                Console.WriteLine("US: " + totalOfThreeNumbers.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))); //US
+                Console.WriteLine("Swedish: " + totalOfThreeNumbers.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")));//Swedish
+                Console.WriteLine("Japanese: " + totalOfThreeNumbers.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));//Japanese 
+                Console.WriteLine("Thai: " + totalOfThreeNumbers.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));//Thai
+
+                Console.ReadLine(); //pause
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something bad happened!");
+
+                Console.WriteLine(ex); //something bad happened
+
+                Console.ReadLine(); //pause
+            }
+
+
+        }
+    }
+}
---
+++ labs/Unit1FunRecommendations/Unit1FunRecommendations/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit1FunRecommendations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+
+            Console.WriteLine("Hello! \n\nHere are your options, pick a numner:\n" +
+                "1 - Action\n" +
+                "2 - Chilling\n" +
+                "3 - Danger\n" +
+                "4 - Good food");
+
+            string chosenActivity = Console.ReadLine();
+
+            if ((chosenActivity == "1") || (chosenActivity == "2") || (chosenActivity == "3") || (chosenActivity == "4"))
+            {
+
+                Console.WriteLine("How many people are you bringing with you?");
+
+                int numberOfPeople = int.Parse(Console.ReadLine());
+
+                string travelMethod = null;
+                if (numberOfPeople == 0)
+                    travelMethod = "sneakers";
+                else if (numberOfPeople > 0 && numberOfPeople <= 4)
+                    travelMethod = "a sedan";
+                else if (numberOfPeople > 4 && numberOfPeople <= 10)
+                    travelMethod = "a VW bus";
+                else if (numberOfPeople > 10)
+                    travelMethod = "an airplane";
+
+                //Console.WriteLine(chosenActivity + " " + numberOfPeople); //test
+
+                string recommendedActivity = null;
+                switch (chosenActivity)
+                {
+                    case "1":
+                        chosenActivity = "action";
+                        recommendedActivity = "stock car racing";
+                        break;
+                    case "2":
+                        chosenActivity = "chilling";
+                        recommendedActivity = "hiking";
+                        break;
+                    case "3":
+                        chosenActivity = "danger";
+                        recommendedActivity = "skydiving";
+                        break;
+                    case "4":
+                        chosenActivity = "good food";
+                        recommendedActivity = "to Taco Bell";
+                        break;
+                    default:
+                        break;
+                }
+
+
+                Console.WriteLine("Okay, if you're in the mood for " + chosenActivity
+                    + ", then you should go to " + recommendedActivity
+                    + " and travel in " + travelMethod
+                    + "\n\nGood bye!");
+
+            }
+            else
+                Console.WriteLine("Hmmm, you entered something wrong. Next time try entering a number.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
