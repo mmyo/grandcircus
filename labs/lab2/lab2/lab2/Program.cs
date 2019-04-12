752821d9a33d0e188b86ed58d33d7f576f5d0b23
Yo <matthewyo@quickenloans.com>
2019-04-11 21:40:19 -0400

    updating lab2

--- labs/lab2/lab2/lab2/Program.cs
+++ labs/lab2/lab2/lab2/Program.cs
@@ -13,17 +13,17 @@ namespace lab2
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to GC Room Detail Generator Thing\n");
-                Console.WriteLine("Enter length");
+                Console.Write("Enter length: ");
                 double length = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter width");
+                Console.Write("Enter width: ");
                 double width = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Area: " + length * width);
 
                 Console.WriteLine("Perimeter: " + (2 * length + 2 * width));
 
-                Console.WriteLine("\nDo you want to continue? y/n");
+                Console.Write("\nDo you want to continue? y/n");
                 userWantsToContinue = Console.ReadLine();
             } while (userWantsToContinue == "y");
 
