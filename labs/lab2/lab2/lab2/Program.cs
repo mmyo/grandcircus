132f5ccc397d4729a4d71606221201670352735c
Yo <matthewyo@quickenloans.com>
2019-04-11 21:36:38 -0400

    updating lab2

--- labs/lab2/lab2/lab2/Program.cs
+++ labs/lab2/lab2/lab2/Program.cs
@@ -5,9 +5,29 @@ namespace lab2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            //
+        {           
+
+            string userWantsToContinue = null;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to GC Room Detail Generator Thing\n");
+                Console.WriteLine("Enter length");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter width");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Area: " + length * width);
+
+                Console.WriteLine("Perimeter: " + (2 * length + 2 * width));
+
+                Console.WriteLine("\nDo you want to continue? y/n");
+                userWantsToContinue = Console.ReadLine();
+            } while (userWantsToContinue == "y");
+
+            //Console.ReadKey();
         }
     }
 }
