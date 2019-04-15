1f434dc81d7f75685c24ac265b258837f16b4447
Yo <matthewyo@quickenloans.com>
2019-04-15 15:59:08 -0400

    exercises

---
+++ class exercises/class0410/ConsoleApp1/exercise21-stringbuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace exercise21_stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userWantsToEnterAnotherWord = null;
+
+            string userSentence = null;
+
+            do
+            {
+                Console.Write("Enter a word: ");
+                string word = Console.ReadLine();
+                userSentence = string.Concat(userSentence, word, " ");
+
+                Console.Write("Do you want to add more words? y/n: ");
+                userWantsToEnterAnotherWord = Console.ReadLine();
+            } while (userWantsToEnterAnotherWord == "y");
+
+            Console.WriteLine(userSentence);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ class exercises/class0410/ConsoleApp1/exercise22-number-in-range/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercise22_number_in_range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Check if this number is within range: ");
+            int checkNumber = int.Parse(Console.ReadLine());
+
+            if (checkNumber >= firstNumber && checkNumber <= secondNumber)
+            {
+                Console.WriteLine($"{checkNumber} is within the range of {firstNumber} and {secondNumber}.");
+            }
+            else
+            {
+                Console.WriteLine($"{checkNumber} is NOT within range of {firstNumber} and {secondNumber}.");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
