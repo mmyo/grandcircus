5dbe3f9ee83256414afe74d038f885fc8e83fc6b
Yo <matthewyo@quickenloans.com>
2019-04-15 14:40:33 -0400

    exercises

---
+++ class exercises/class0410/ConsoleApp1/exercise18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (int i = 0; i <= userNumber; i++)
+            {
+                total = total + i;
+            }
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+
+        }
+    }
+}
