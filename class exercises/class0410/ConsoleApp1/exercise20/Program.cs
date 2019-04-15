262062ba4f851e2b5abde224dd578ad317a93472
Yo <matthewyo@quickenloans.com>
2019-04-15 15:30:16 -0400

    exercises

---
+++ class exercises/class0410/ConsoleApp1/exercise20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            int total = 1;
+
+            for (int i = userNumber - 2; i <= userNumber; i++)
+            {
+                total = total * i;
+            }
+
+            Console.WriteLine($"The product of {userNumber}, {userNumber-1}, and {userNumber-2} is {total}");
+            Console.ReadKey();
+        }
+    }
+}
