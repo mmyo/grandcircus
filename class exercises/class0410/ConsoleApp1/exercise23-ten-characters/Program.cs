54640f2e38756756a745a44f20df6602fb1493a7
Yo <matthewyo@quickenloans.com>
2019-04-15 16:04:02 -0400

    exercises

---
+++ class exercises/class0410/ConsoleApp1/exercise23-ten-characters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise23_ten_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter some text: ");
+            string userText = Console.ReadLine();
+
+            Console.WriteLine(userText.Substring(0, 10));
+
+            Console.ReadKey();
+        }
+    }
+}
