a55a295cdd2fe8fd8df987493b09dba5d9b35174
Yo <matthewyo@quickenloans.com>
2019-04-15 15:08:49 -0400

    exercises

---
+++ class exercises/class0410/ConsoleApp1/exercise19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int total = 0;
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                total = total + i;
+               
+            }
+
+            Console.WriteLine($"Sum from {firstNumber} to {secondNumber} is {total}.");
+
+            Console.ReadKey();
+        }
+    }
+}
