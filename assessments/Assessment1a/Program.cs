df1c4351f98eb770453687d8eec90e43ad718ccb
Yo <matthewyo@quickenloans.com>
2019-04-18 19:11:35 -0400

    assessment 1

---
+++ assessments/Assessment1a/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Assessment1a
+{
+    class Challenge
+    {
+        static void Main(string[] args)
+        {
+            //Tests
+            Console.WriteLine("Test Method 1 \nEnter grade");
+            double gradeIsPassingTest = double.Parse(Console.ReadLine());
+            Console.WriteLine(IsPassing(gradeIsPassingTest));
+
+            Console.WriteLine("\nTest Method 2 \nEnter grade");
+            double gradeIsPassingAndEvenOddTest = double.Parse(Console.ReadLine());
+            Console.WriteLine(IsPassingAndEvenOrOdd(gradeIsPassingAndEvenOddTest));
+
+            Console.WriteLine("\nTest Method 3\ngrade1: ");
+            double grade1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("grade2: ");
+            double grade2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("grade3: ");
+            double grade3 = double.Parse(Console.ReadLine());
+
+            var average = AverageGrades(grade1, grade2, grade3);
+            Console.WriteLine($"Average: {average}");
+
+            Console.ReadLine();
+        }
+
+        static bool IsPassing(double grade)
+        {
+
+            if (grade > 65)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+
+        static double AverageGrades(double grade1, double grade2, double grade3)
+        {
+            return (grade1 + grade2 + grade3) / 3;
+        }
+
+        static string IsPassingAndEvenOrOdd(double grade)
+        {
+            double remainder = grade % 2;
+
+            if (grade > 65 && remainder > 0)
+            {
+                return "Passing and odd";
+            }
+            else if (grade > 65 && remainder == 0)
+            {
+                return "Passing and even";
+            }
+            else if (grade <= 65 && remainder > 0)
+            {
+                return "Failing and odd";
+            }
+            else if (grade <= 65 && remainder == 0)
+            {
+                return "Failing and even";
+            }
+            else
+            {
+                return "Something bad happened";
+            }
+
+        }
+    }
+}
---
+++ labs/lab3/lucero/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hi friend, what's your name: ");
+            var userInputName = Console.ReadLine();
+
+            string didUserContinue = null;
+            do
+            {
+
+                Console.Write($"{userInputName}, please enter a number between 1 and 100: ");
+                var userNumberInput = Console.ReadLine();
+
+                var number = 0;
+                if (int.TryParse(userNumberInput, out number))
+                {
+
+                    var response = NumberEvenOrOdd(number);
+
+                    if (number >= 1 && number <= 100)
+                    {
+                        Console.WriteLine(response);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{userInputName}, you did not follow the directions. Nice try, but try again!!");
+                    }
+
+                }
+
+                else
+                {
+                    Console.WriteLine($"{userInputName},you didn't enter a number!!!");
+                }
+
+                Console.Write("Continue?(y/n)");
+
+                didUserContinue = Console.ReadLine();
+
+            } while (didUserContinue.ToLower() != "n");
+
+        }
+
+
+
+        static string NumberEvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+
+                if (number >= 2 && number <= 25)
+                {
+                    return "Even and less than 25";
+                }
+                else if (number >= 26 && number <= 60)
+                {
+                    return "Even";
+                }
+                else
+                {
+                    return $"{number} and Even";
+                }
+
+
+            }
+            else
+            {
+                if (number >= 60)
+                {
+                    return $"{number} and Odd";
+                }
+                else
+                {
+                    return $"{number} and Odd";
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
