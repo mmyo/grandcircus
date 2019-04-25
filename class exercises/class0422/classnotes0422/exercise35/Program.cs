30d1c58b624e83fd5060d297afc276dc13c37b61
Yo <matthewyo@quickenloans.com>
2019-04-25 18:31:21 -0400

    exercises

---
+++ class exercises/class0422/classnotes0422/exercise35/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animalArray = new string[] { "cow", "elephant", "jaguar", "horse", "crow" };
+
+            Console.Write("Enter two indcies: ");
+
+            string[] userIndicies = Console.ReadLine().Split(" ");
+
+            Console.WriteLine(userIndicies[0].ToString());
+            Console.WriteLine(userIndicies[1].ToString());
+
+
+            //if (int.TryParse(userIndicies[0] && userIndex >= 0 && userIndex < animalArray.Length)
+            //{
+
+            //}
+
+
+            Console.ReadLine();
+        }
+    }
+}
