bddbb436417b3dcb65b92dcbd5021960664d5889
Yo <matthewyo@quickenloans.com>
2019-05-01 17:03:45 -0400

    updated assessment2 based on feedback

--- assessments/Assessment2/Assessment2/Assessment2b/Program.cs
+++ assessments/Assessment2/Assessment2/Assessment2b/Program.cs
@@ -23,8 +23,32 @@ namespace Assessment2b
                 }
             }
 
-            Console.WriteLine("Here's your array:");
+            Console.WriteLine("Here's your array (for and if loop):");
             foreach (var item in userArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            int counter=0;
+            int[] userArray2 = new int[10];
+            while (counter < userArray2.Length)
+            {
+                Console.WriteLine("Enter a positive odd integer: ");
+
+                if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber > 0 && (userNumber % 2 > 0))
+                {
+                    userArray2[counter] = userNumber;
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                }
+            }
+
+            Console.WriteLine("Here's your array (while loop):");
+            foreach (var item in userArray2)
             {
                 Console.WriteLine(item);
             }
