eebe317ace74ca531aaaabb8553873dd29d9e397
Yo <matthewyo@quickenloans.com>
2019-05-03 18:03:08 -0400

    exercises

---
+++ class exercises/class0429/classnotes0429/exercise24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace exercise24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            var userString = Console.ReadLine();
+            StringBuilder newString = new StringBuilder();
+            string newWord2 = null;
+            var userStringPosition = userString.Length - 10 ;
+
+            if (userString.Length > 10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    newWord2 = newWord2 + userString[userStringPosition];
+                    newString.Append(userString[userStringPosition]);
+                    userStringPosition++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a string longer than 10 characters");
+            }
+
+            Console.WriteLine($"Using string concatenation: {newWord2}");
+            Console.WriteLine($"Using string builder: {newString}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class exercises/class0429/classnotes0429/exercise25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercise25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a sentence: ");
+            var userSentence = Console.ReadLine();
+
+            string[] sentenceArray = userSentence.Split(" ");
+
+            foreach (var item in sentenceArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
