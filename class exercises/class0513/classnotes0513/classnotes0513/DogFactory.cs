c71f1af3c6907403cb11539a2f3438d4eb851506
Matthew Yo <matthewyo@quickenloans.com>
2019-05-13 21:57:10 -0400

    class notes

---
+++ class exercises/class0513/classnotes0513/classnotes0513/DogFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classnotes0513
+{
+    interface DogFactory
+    {
+    }
+}
---
+++ class exercises/class0513/classnotes0513/classnotes0513/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classnotes0513
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factory pattern
+            var dogFactory = new DogFactory();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
