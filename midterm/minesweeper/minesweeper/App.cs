e95fd822909d4d922aeaebc29fd2b6d984467016
Matthew Yo <matthewyo@quickenloans.com>
2019-05-23 14:55:16 -0400

    minesweeper

--- midterm/minesweeper/minesweeper/App.cs
+++ midterm/minesweeper/minesweeper/App.cs
@@ -8,43 +8,27 @@ namespace minesweeper
     {
         public static void Game()
         {
-            //var gameBoard = new Board();
-            //GenerateGameBoard(gameBoard, 4);
 
-            int boardMax = 10;
-            var board = GenerateGameBoard(boardMax);
-
-            //manually generate some mines
-            var listOfMines = new List<Cell>();
-            //randomly generate mine coordinates.at board[x,y] set Cell.IsMine=true 
-            board[0, 0].IsMine = true; 
-            board[1, 1].IsMine = true;
-            board[2, 2].IsMine = true;
-            board[3, 3].IsMine = true;
-            board[5, 5].IsMine = true;
-
-
-            //calculate proximity to mines
-            GenerateProximityCounter(board, board[0, 0]);
-            GenerateProximityCounter(board, board[1, 1]);
-            GenerateProximityCounter(board, board[2, 2]);
-            GenerateProximityCounter(board, board[3, 3]);
-            GenerateProximityCounter(board, board[5, 5]);
-
-            DrawBoard(board, false, true);
+            var gameBoard = new Board(SetDifficultyLevel());
 
             bool keepGuessing = true;
             do
             {
+                Console.Clear();
+                gameBoard.DisplayEntireBoard();
+                Console.WriteLine();
+                gameBoard.DisplayBoard();
+
                 Console.WriteLine("Guess: ");
                 Console.Write("X: ");
                 var userX = int.Parse(Console.ReadLine());
                 Console.Write("Y: ");
                 var userY = int.Parse(Console.ReadLine());
 
-                GuessCell(board, userX, userY);
+                GuessCell(gameBoard, userX, userY);
 
                 Console.Write("\nGuess again? y/n: ");
+
                 if (Console.ReadLine() == "n")
                 {
                     keepGuessing = false;
@@ -52,74 +36,24 @@ namespace minesweeper
 
             } while (keepGuessing == true);
 
-
         }
 
-        public static void GenerateGameBoard(Board board, int boardMax)
+        public static Enum SetDifficultyLevel()
         {
 
-            for (int x = 0; x < boardMax; x++)
+            Console.Write("Difficulty Level? (1,2,3) : ");
+
+            if (Enum.TryParse<DiffcultyLevel>(Console.ReadLine(), out var level))
             {
-
-                for (int y = 0; y < boardMax; y++)
-                {
-                    var cell = new Cell() { XCoordinate = x };
-                    cell.YCoordinate = y;
-                    board.ListOfCells.Add(cell);
-                }
-
+                return level;
             }
+            return null;
+
         }
 
-        public static Cell[,] GenerateGameBoard(int boardMax)
+        public static void GuessCell(Board board, int userX, int userY)
         {
-            Cell[,] gameBoardArray = new Cell[boardMax, boardMax];
-
-            for (int x = 0; x < boardMax; x++)
-            {
-
-                for (int y = 0; y < boardMax; y++)
-                {
-                    var cell = new Cell() { XCoordinate = x };
-                    cell.YCoordinate = y;
-                    gameBoardArray[x, y] = cell;
-                }
-
-            }
-
-            return gameBoardArray;
-        }
-
-        public static void GenerateMines(Cell[,] board, int x, int y)
-        {
-            foreach (var cell in board)
-            {
-                if (cell.XCoordinate == 1 && cell.YCoordinate == 1)
-                {
-                    cell.IsMine = true;
-                }
-            }
-        }
-
-        public static void GenerateProximityCounter(Cell[,] board, Cell mine)
-        {
-
-            for (int x = mine.XCoordinate -1 ; x <= mine.XCoordinate + 1 ; x++)
-            {
-                for (int y = mine.YCoordinate -1 ; y <= mine.YCoordinate + 1; y++)
-                {
-                    if (x >= 0 && y >= 0 && x < board.GetLength(0) && y < board.GetLength(1))
-                    {
-                        board[x, y].ProximityCounter++;
-
-                    }
-                }
-            }
-        }
-
-        public static void GuessCell(Cell[,] board, int userX, int userY)
-        {
-            var cellGuessed = board[userX, userY];
+            var cellGuessed = board.BoardArray[userX, userY];
             cellGuessed.UserGuessed = true;
             if (cellGuessed.IsMine == true)
             {
@@ -130,58 +64,6 @@ namespace minesweeper
                 Console.WriteLine($"Mines nearby: {cellGuessed.ProximityCounter}");
             }
 
-        }
-
-        public static void DrawBoard(Cell[,] board, bool listAllCoordinates, bool showAll)
-        {
-            int rowLength = board.GetLength(0);
-            int colLength = board.GetLength(1);
-
-            if (showAll == true)
-            {
-                for (int i = rowLength - 1; i >= 0; i--)
-                {
-                    for (int j = 0; j < colLength; j++)
-                    {
-                        var currentCell = board[j, i];
-                        if (currentCell.IsMine == true)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Gray;
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write(" M ");
-                            Console.ResetColor();
-                        }
-                        else if (currentCell.ProximityCounter > 0)
-                        {
-                            Console.BackgroundColor = ConsoleColor.White;
-                            Console.Write($" {currentCell.ProximityCounter} ");
-                            Console.ResetColor();
-
-                        }
-                        else
-                        {
-                            Console.BackgroundColor = ConsoleColor.White;
-                            Console.Write($"   ");
-                            Console.ResetColor();
-
-                        }
-                        //board[i,j].IsMine == true
-                    }
-                    Console.Write(Environment.NewLine);
-                }
-            }
-
-           
-            if (listAllCoordinates.Equals(true))
-            {
-                for (int x = 0; x < board.GetLength(0); x++)
-                {
-                    for (int y = 0; y < board.GetLength(1); y++)
-                    {
-                        Console.WriteLine($"{board[x, y].XCoordinate}, {board[x, y].YCoordinate}, {board[x, y].IsMine}, {board[x, y].ProximityCounter}");
-                    }
-                }
-            }
         }
  
     }
--- midterm/minesweeper/minesweeper/Board.cs
+++ midterm/minesweeper/minesweeper/Board.cs
@@ -6,18 +6,165 @@ namespace minesweeper
 {
     public class Board
     {
-        public List<Cell> ListOfCells { get; set; }
 
         public Cell[,] BoardArray { get; set; }
 
         public int BoardSize { get; set; }
 
-        public Board(int boardSize)
+        public int NumberOfMines { get; set; }
+
+        public Enum DifficultyLevel { get; set; }
+
+        public Board(Enum difficulty)
         {
-            //ListOfCells = new List<Cell>();
-            BoardSize = boardSize;
+            CalcBoardSizeAndNumberOfMines(difficulty);
+            PopulateGameBoard();
+            GenerateMines();
+
         }
-       
+
+        public void CalcBoardSizeAndNumberOfMines(Enum difficulty)
+        {
+            switch (difficulty)
+            {
+                case DiffcultyLevel.Easy:
+                    BoardSize = 9;
+                    NumberOfMines = 10;
+                    break;
+                case DiffcultyLevel.Medium:
+                    BoardSize = 16;
+                    NumberOfMines = 40;
+                    break;
+                case DiffcultyLevel.Hard:
+                    BoardSize = 20;
+                    NumberOfMines = 99;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void PopulateGameBoard()
+        {
+            BoardArray = new Cell[BoardSize, BoardSize];
+
+            for (int x = 0; x < BoardSize; x++)
+            {
+                for (int y = 0; y < BoardSize; y++)
+                {
+                    BoardArray[x, y] = new Cell() { XCoordinate = x, YCoordinate = y }; ;
+                }
+            }
+        }
+
+        public void DisplayEntireBoard()
+        {
+            for (int i = BoardArray.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < BoardArray.GetLength(1); j++)
+                {
+                    var currentCell = BoardArray[j, i];
+                    if (currentCell.IsMine == true)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" M ");
+                        Console.ResetColor();
+                    }
+                    else if (currentCell.ProximityCounter > 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write($" {currentCell.ProximityCounter} ");
+                        Console.ResetColor();
+
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write($"   ");
+                        Console.ResetColor();
+
+                    }
+                    //board[i,j].IsMine == true
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        public void GenerateProximityCounter(Cell mine)
+        {
+            for (int x = mine.XCoordinate - 1; x <= mine.XCoordinate + 1; x++)
+            {
+                for (int y = mine.YCoordinate - 1; y <= mine.YCoordinate + 1; y++)
+                {
+                    if (x >= 0 && y >= 0 && x < BoardSize && y < BoardSize)
+                    {
+                        BoardArray[x, y].ProximityCounter++;
+                    }
+                }
+            }
+        }
+
+        public void GenerateMines()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < NumberOfMines; i++)
+            {
+                int x = rand.Next(0, BoardSize - 1);
+                int y = rand.Next(0, BoardSize - 1);
+
+                BoardArray[x, y].IsMine = true;
+
+                GenerateProximityCounter(BoardArray[x, y]);
+            }
+        }
+
+        public void DisplayBoard()
+        {
+
+            for (int i = BoardArray.GetLength(0) - 1; i >= 0; i--)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+
+                for (int j = 0; j < BoardArray.GetLength(1); j++)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+
+                    var currentCell = BoardArray[j, i];
+                    if (currentCell.UserGuessed == true && currentCell.ProximityCounter <= 0 && currentCell.IsMine == false)
+                    {
+
+                        //Console.ForegroundColor = ConsoleColor.Red;
+                        //Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write(" x ");
+                        //Console.ResetColor();
+                    }
+                    else if (currentCell.UserGuessed == true && currentCell.ProximityCounter > 0 && currentCell.IsMine == false)
+                    {
+                        //Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write($" {currentCell.ProximityCounter} ");
+                        //Console.ResetColor();
+
+                    }
+                    else if (currentCell.UserGuessed == true && currentCell.IsMine == true)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" M ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                        
+                    }
+                    //board[i,j].IsMine == true
+                }
+                Console.Write(Environment.NewLine);
+            }
+            Console.ResetColor();
+        }
     }
 
 
