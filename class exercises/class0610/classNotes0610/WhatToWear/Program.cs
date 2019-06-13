1542a76f1b547260eddee6b2dc53e57fbf84dc59
Matthew Yo <matthewyo@quickenloans.com>
2019-06-12 21:48:09 -0400

    html

--- class exercises/class0610/classNotes0610/WhatToWear/Program.cs
+++ class exercises/class0610/classNotes0610/WhatToWear/Program.cs
@@ -10,7 +10,15 @@ namespace WhatToWear
             Console.Write("What's the event? ");
             var eventType = Enum.Parse(typeof(EventEnum), Console.ReadLine());
 
-            Console.WriteLine(eventType);
+            Console.Write("What's the temp? ");
+            var temperature = Enum.Parse(typeof(TempEnum), Console.ReadLine());
+
+            var eventWear = new EventTypeWear();
+            var tempWear = new TemperatureWear();
+
+
+
+            Console.WriteLine(eventWear.PickMyEventClothes(eventType));
 
             //casual "something comfy" semi-formal "a polo" formal "a suit"
             //< 54 degrees "a coat", 54-70 "a jacket", > 70 "no jacket" 
---
+++ html101/main.js
@@ -0,0 +1,4 @@
+function sayHi(){
+    alert("hello!");
+    console.log("im a console log");
+}
